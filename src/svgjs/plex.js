518418bed585ed6bce918c94a102abbe8443face
Dan Pastusek <dan@hexial.com>
2023-10-02 05:24:40 +0100

    fix company logos, add plex

---
+++ src/svgjs/plex.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+const SvgPlex = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 100 100"
+    {...props}
+  >
+    <path
+      fill="#EBAF00"
+      d="M49.907 50.123 16 0h34.89l33.66 50.123L50.89 100H16l33.907-49.877Z"
+    />
+  </svg>
+);
+export default SvgPlex;
