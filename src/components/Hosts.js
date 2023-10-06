91682f92a1ea58d2a8deeff1f24d152049edd050
Dan Pastusek <dan@hexial.com>
2023-10-06 22:19:32 +0100

    remove streamedian entirely

--- src/components/Hosts.js
+++ src/components/Hosts.js
@@ -14,9 +14,6 @@ import { useEffect, useState } from "react";
 import * as svg from "@/svgjs/index.js";
 import oui from "../oui/oui.json";
 
-import * as streamedian from "streamedian/player.js";
-// import WebsocketTransport from 'wsp/transport/websocket';
-// import RTSPClient from 'wsp/client/rtsp/client';
 
 const fields = [
   "ip",
@@ -383,43 +380,6 @@ export default function Hosts({ connected, hosts }) {
 
   const iconHosts = hosts.filter((h) => h.mini);
   const tableHosts = hosts.filter((h) => !h.mini);
-
-  useEffect(() => {
-    let mediaElement = rtsp.attach(document.getElementById("test_video"));
-    let player = new streamedian.WSPlayer(mediaElement, {
-      // url: `${STREAM_URL}`,      // overrides mediaElement's sources
-      modules: [
-        {
-          // client module constructor. Should be subclass or BaseClient. RTSPClient by default
-          // client: RTSPClient,
-          transport: {
-            // client module constructor. Should be subclass or BaseTransport. WebsocketTransport by default
-            // constructor: WebsocketTransport,
-            options: {
-              // address of websocket proxy described below. ws${location.protocol=='https:'?'s':''}://${location.host}/ws/ by default
-              socket: "ws://websocket_proxy_address/ws",
-              // function called player exceptions
-              errorHandler(e) {
-                alert(`Failed to start player: ${e.message}`);
-              },
-              // function to get credentials for protected streams
-              queryCredentials() {
-                return new Promise((resolve, reject) => {
-                  let c = prompt("input credentials in format user:password");
-                  if (c) {
-                    this.setCredentials.apply(this, c.split(":"));
-                    resolve();
-                  } else {
-                    reject();
-                  }
-                });
-              },
-            },
-          },
-        },
-      ],
-    });
-  }, []);
 
   return (
     <>
